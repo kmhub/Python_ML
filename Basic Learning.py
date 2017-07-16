a6882e7146026dba31d2d916fda35e3d5c1d27b8
KM <kamalmittal12@gmail.com>
2017-07-16 08:03:40 -0400

    include linear regression

--- Basic Learning.py
+++ Basic Learning.py
@@ -12,6 +12,21 @@ Created on Sun Jul  9 01:22:06 2017
 #Tuples() are immutable, fixed length vs. Lists [] are the mutable variable length
 #Pandas uses NaN to cover missing values
 
+#Python Data science libraries
+#• IPython
+#• Enhanced interpreter
+#• NumPy
+#• Matlab type stuff
+#• Ndarray object
+#• Matplotlib
+#• Plotting in 2D originally (now some 3D)
+#• Pandas
+#• Dataframe class, Time series analysis
+#• Stat Models
+#• Basic Regression
+#• Scikit Learn
+#• Machine learning classification
+
 x= "hello world"
 print(x.capitalize())
 print("Hello world")
@@ -484,3 +499,96 @@ states["Income_bins"] = pd.cut(states.Income,3,labels=["low","med","high"])
 # Histograms
 states.Income.plot(kind="hist")
 states.Income.hist(color='k', alpha=0.5, bins=50)
+
+
+-------------------------------------
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
+## Setting Working directory
+import os
+## get working directory
+os.getcwd()
+## set working directory
+path = 'C:\\Users\\kammittal\\Desktop\\6. Technical Toolkit\\1. Python + ML\\2. Python Training Material'
+os.chdir(path)
+# 1) Import the dataset using pandas.
+tips = pd.read_csv(r".\tips.csv")
+
+# 2) Answer the following questions using summary statistics and grouped analyses 
+# (no linear modeling yet)
+# a. Examine the dataset first, looking at the distribution of all variables 
+# (frequency tables for categorical variables 
+# and mean/quantiles for continuous)
+tips.describe()
+
+# b. Examine the distribution of total tip using a histogram. 
+# Is this distribution symmetric? How would you describe it?
+# plot a histogram
+tips.tip.plot(kind="hist")
+
+# c. Do smokers tip more than non-smokers on average?
+tips.tip.mean()
+tips_only = tips.tip
+tips.query('smoker == "Yes"').tip.mean()
+tips.query('smoker == "No"').tip.mean()
+tips.groupby(["smoker"]).mean()
+tips.groupby(["sex", "smoker"]).mean()
+    
+# d. What about females versus males?
+tips.groupby(["sex"]).mean()
+
+# e. What is the trend in tip average tip size across day of week?
+tips.groupby(["day"]).mean()
+
+# f. Are tips typically higher over lunch or dinner?
+tips.groupby(["time"]).mean()
+
+# g. Is the size of the party correlated with the total tip?
+tips.groupby(["size"]).mean()
+
+# h. Perhaps it is more accurate to consider the tip percentage rather than 
+# the total tip. Create a new variable which is the tip percentage, 
+# then repeat c-g above for tip percentage.
+tips['tip_pcn'] =  100 * (tips['tip'] / tips['total_bill'])
+tips.query('smoker == "Yes"').tip_pcn.mean()
+tips.query('smoker == "No"').tip_pcn.mean()
+tips.groupby(["smoker"]).mean()
+tips.groupby(["sex", "smoker"]).mean()
+    
+# females versus males?
+tips.groupby(["sex"]).mean()
+
+# trend in tip average tip size across day of week?
+tips.groupby(["day"]).mean()
+
+# Are tips typically higher over lunch or dinner?
+tips.groupby(["time"]).mean()
+
+# Is the size of the party correlated with the total tip?
+tips.groupby(["size"]).mean()
+
+# i. Is there any correlation between bill size and tip percentage?
+sns.jointplot(x="total_bill", y="tip_pcn", data=tips, kind="reg")
+sns.lmplot(x="total_bill", y="tip_pcn", data=tips)
+
+# 3. Construct a linear model with tip percentage as the outcome. Based on 
+# your analyses above, construct what you think is the best multivariate 
+# linear model for tip percentage.
+# use ordinary least squares model
+tips_lm1=smf.ols('tip_pcn~total_bill+C(smoker)', data=tips).fit() #Specify C for Categorical
+print(tips_lm1.summary())
+tips_lm1=smf.ols('tip_pcn~C(sex)+total_bill+C(smoker)+size+C(time)+C(day)', data=tips).fit()
+print(tips_lm1.summary())
+
+# Looking at the summary
+# coef - the various coefficients for each predictor
+# std err - the estimate of standard deviation of the coefficient
+# t - t statistic
+# P>|t| - p-value
+# last is confidence interval
